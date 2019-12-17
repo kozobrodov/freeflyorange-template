f770f270575fad4dc054e92fc6e8c07344c5faac
Eugene Kozobrodov <eugene.kozobrodov@gmail.com>
2019-12-18 00:17:21 +0300

    Add "copy" button for coordinates

--- js/common.js
+++ js/common.js
@@ -1,6 +1,6 @@
 var top_menu_height = 0;
 jQuery(function($) {
-        $(document).ready( function() {
+    $(document).ready( function() {
 
         // load google map
         var script = document.createElement('script');
@@ -26,4 +26,20 @@ function initialize() {
     };
 
     var map = new google.maps.Map(document.getElementById('map-canvas'),  mapOptions);
+
+    // Init "copy coordinates"
+    // In case of JS turned off, this button won't be shown
+    $('.copyable').each(function(index, element) {
+        $('<i>')
+            .addClass('fas fa-copy copy')
+            .click(function(e) {
+                var text = $(e.target).parent().text();
+                var $temp = $("<input>");
+                $("body").append($temp);
+                $temp.val(text).select();
+                document.execCommand("copy");
+                $temp.remove();
+            })
+            .appendTo(element);
+    });
 }
