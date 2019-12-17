249ca4ec284cffd4fff4542f124910404bf7261a
Eugene Kozobrodov <eugene.kozobrodov@gmail.com>
2019-12-18 00:32:44 +0300

    Remove google maps

--- js/common.js
+++ js/common.js
@@ -1,14 +1,6 @@
 var top_menu_height = 0;
 jQuery(function($) {
     $(document).ready( function() {
-
-        // load google map
-        var script = document.createElement('script');
-        script.type = 'text/javascript';
-        script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' +
-            'callback=initialize';
-        document.body.appendChild(script);
-
         //gallery light box setup
         $('a.colorbox').colorbox({
                                     rel: function(){
@@ -20,13 +12,6 @@ jQuery(function($) {
 });
 
 function initialize() {
-    var mapOptions = {
-      zoom: 12,
-      center: new google.maps.LatLng(16.8451789,96.1439764)
-    };
-
-    var map = new google.maps.Map(document.getElementById('map-canvas'),  mapOptions);
-
     // Init "copy coordinates"
     // In case of JS turned off, this button won't be shown
     $('.copyable').each(function(index, element) {
