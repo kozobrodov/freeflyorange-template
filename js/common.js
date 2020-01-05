8bd4bf221b30a7dc1532ca760a140598ba3a9345
Eugene Kozobrodov <eugene.kozobrodov@gmail.com>
2020-01-05 17:51:18 +0300

    Remove useless stuff

--- js/common.js
+++ js/common.js
@@ -6,22 +6,6 @@ jQuery(function($) {
 });
 
 function initialize() {
-    // Init "copy coordinates"
-    // In case of JS turned off, this button won't be shown
-    $('.copyable').each(function(index, element) {
-        $('<i>')
-            .addClass('fas fa-copy copy')
-            .click(function(e) {
-                var text = $(e.target).parent().text();
-                var $temp = $("<input>");
-                $("body").append($temp);
-                $temp.val(text).select();
-                document.execCommand("copy");
-                $temp.remove();
-            })
-            .appendTo(element);
-    });
-
     //gallery light box setup
     $('a.colorbox').colorbox({
         rel: function() {
